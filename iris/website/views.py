4199ab6c85aca1a50973d6c530dcb8d017eb6934
Dannielle <00danstar@gmail.com>
2025-05-08 10:08:48 +1200

    Working on nav

--- iris/website/views.py
+++ iris/website/views.py
@@ -2,10 +2,10 @@ from flask import Blueprint, render_template
 
 views = Blueprint('views', '__name__')
 
-@views.route('/')
-def portalSelect():
+#@views.route('/')
+#def portalSelect():
 
-   return render_template("portalSelect.html")
+#   return render_template("portalSelect.html")
 
 #For now, just comment out ^ if you want to see the navBase
 @views.route('/')
