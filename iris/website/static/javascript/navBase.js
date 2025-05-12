5454b274664f046ba1e108ba0787b0ce0d145453
Dannielle <00danstar@gmail.com>
2025-05-12 14:06:55 +1200

    Working on the top navigation bar

--- iris/website/static/javascript/navBase.js
+++ iris/website/static/javascript/navBase.js
@@ -1,28 +1,28 @@
 /* Toggle nav state */
 function openNav() {
     const sideNav = document.getElementById("mySideNav");
-    const container = document.getElementById("container");
+    const content = document.getElementById("content");
 
     // Removes collapsed class to restore styling
     sideNav.classList.remove("collapsed");
-    container.classList.remove("navCollapsed");
+    content.classList.remove("navCollapsed");
 
     // Resetting any inline styles the might interfere
     sideNav.style.width = "";
-    container.style.marginLeft = "";
+    content.style.marginLeft = "";
 }
 
-/* Changing side nav's width and the container's left margin when nav is closed */
+/* Changing side nav's width and the content's left margin when nav is closed */
 function closeNav() {
    const sideNav = document.getElementById("mySideNav");
-    const container = document.getElementById("container");
+    const content = document.getElementById("content");
 
 
     // Adds collapsed class
     sideNav.classList.add("collapsed");
-    container.classList.add("navCollapsed");
+    content.classList.add("navCollapsed");
 
     // Resetting any inline styles that might interfere
     sideNav.style.width = "";
-    container.style.marginLeft = "";
+    content.style.marginLeft = "";
 }
