07515ad9e60fad6f27d561c0ac8bc6733de84e39
supertop24 <hendrawankusumahalim@gmail.com>
2025-05-15 14:14:00 +1200

    database set up

--- iris/website/__init__.py
+++ iris/website/__init__.py
@@ -1,11 +1,20 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from config import Config
 
-
-def create_app():
+db = SQLAlchemy()
+def create_app(config_class='website.config.DevelopmentConfig'):
     app=Flask(__name__)
-    app.config['SECRET_KEY']='keykeykey'
+    app.config.from_object(config_class)
+
+    # Initialize the db with the app
+    db.init_app(app)
+    BASEDIR = os.path.abspath(os.path.dirname(__file__))
+    if not os.path.exists(os.path.join(BASEDIR, 'database.db')):
+        with app.app_context():
+            db.create_all()  # Create tables in the database
+
+
 
     from .views import views
     from .auth import auth
